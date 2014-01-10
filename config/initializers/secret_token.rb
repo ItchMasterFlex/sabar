f3ff1c43a86305187db6d210b8489e75d2dc801f
Sabar Dasgupta <sabar@orchive.com>
2014-01-09 23:26:37 -0500

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sabar::Application.config.secret_key_base = 'dfd978f2d061eb9ff2cbd220c5b300337fa613116b48a5cb6b67d1297808ada301eea38567adcdf0fb79681aca8bdcc8cda67423128f7d48e6387a405ad5d901'
